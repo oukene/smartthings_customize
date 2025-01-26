ac8022bbdce402bec3d16ea0d7fa0e0e8170a761
ouk <ouk@rhaon.co.kr>
2025-01-26 09:46:27 +0900

    Revert "- 토큰 갱신문제 수정"
    This reverts commit c918e3d3f94b9d97f5ef4902b4b0373a51e767b2.

--- custom_components/smartthings_customize/__init__.py
+++ custom_components/smartthings_customize/__init__.py
@@ -444,9 +444,6 @@ class DeviceBroker:
                     CONF_REFRESH_TOKEN: self._token.refresh_token,
                 },
             )
-            for device in self.devices:
-                device._api._token = self._token.access_token
-                
             _LOGGER.debug(
                 "Regenerated refresh token for installed app: %s",
                 self._installed_app_id,
