fa11f1add3c5a1bcbcb963c19f0d48b701116b1e
ouk <ragcarib@naver.com>
2025-12-16 14:50:52 +0900

    오류수정
    Options Flow 오류수정

--- custom_components/smartthings_customize/config_flow.py
+++ custom_components/smartthings_customize/config_flow.py
@@ -314,7 +314,7 @@ class OptionsFlowHandler(OptionsFlow):
 
     def __init__(self, config_entry) -> None:
         """Initialize options flow."""
-        self.config_entry = config_entry
+        #self.config_entry = config_entry
 
     async def async_step_init(self, user_input: dict[str, Any] = None) -> dict[str, Any]:
         errors: dict[str, str] = {}
